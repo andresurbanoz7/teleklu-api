e8075534214b55598c643fbf3c0eef30ee507120
andresurbanoz7 <121873596+andresurbanoz7@users.noreply.github.com>
2025-04-23 18:03:39 -0400

    Update server.py

--- server.py
+++ server.py
@@ -38,5 +38,8 @@ async def activar_teleclub():
             return {"status": "Error", "detalle": str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 8080))
+app.run(host='0.0.0.0', port=port)
+
 
